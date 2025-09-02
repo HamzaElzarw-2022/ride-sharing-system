56ec6e5a013dab137c02b6746c422598304d6d08
Hamza Elzarw <hamzaelzarw@gmail.com>
2025-09-03 01:16:15 +0300

    Add MapMetadata entity and repository, and implement map loading in MapService

--- core/src/main/java/com/rss/backend/map/service/MapService.java
+++ core/src/main/java/com/rss/backend/map/service/MapService.java
@@ -1,11 +1,18 @@
 package com.rss.backend.map.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rss.backend.map.entity.Edge;
+import com.rss.backend.map.entity.MapMetadata;
 import com.rss.backend.map.entity.Node;
 import com.rss.backend.map.repository.EdgeRepository;
+import com.rss.backend.map.repository.MapMetadataRepository;
 import com.rss.backend.map.repository.NodeRepository;
+import jakarta.annotation.PostConstruct;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -13,11 +20,54 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MapService {
     private final NodeRepository nodeRepository;
     private final EdgeRepository edgeRepository;
+    private final MapMetadataRepository metadataRepository;
+    private final ObjectMapper objectMapper;
+
+    private static final String DEFAULT_MAP_PATH = "map.json";
+
+    @PostConstruct
+    public void loadMapOnStartup() {
+        try {
+            ClassPathResource resource = new ClassPathResource(DEFAULT_MAP_PATH);
+            MapMetadata metaData = metadataRepository.findByKey("meta").orElse(null);
+
+            if (resource.exists()) {
+                log.info("Map file found at: {}", DEFAULT_MAP_PATH);
+                JsonNode rootNode = objectMapper.readTree(resource.getInputStream());
+                String version = rootNode.get("version").asText();
+
+                if(metaData != null && metaData.getVersion().equals(version)) {
+                    log.info("Map up to date (version: {})", version);
+                    return;
+                } else if(metaData == null) {
+                    metaData = new MapMetadata();
+                    metaData.setKey("meta");
+                }
+
+                metaData.setVersion(version);
+                metaData.setLongitude(rootNode.get("longitude").asInt());
+                metaData.setLatitude(rootNode.get("latitude").asInt());
+                metaData.setUpdatedAt(java.time.LocalDateTime.now());
+                metadataRepository.save(metaData);
+
+                log.info("Map version changed to {}. Updating map...", version);
+                updateMap(rootNode.get("edges"), rootNode.get("nodes"));
+
+            } else {
+                log.info("Map file not found at: {}. Using existing map data.", DEFAULT_MAP_PATH);
+                if(metaData == null)
+                    log.error("No map metadata found. The map might be empty.");
+            }
+        } catch (IOException e) {
+            log.error("Error loading map file: {}", e.getMessage(), e);
+        }
+    }
 
     public void updateMap(JsonNode jsonEdges, JsonNode jsonNodes) {
         edgeRepository.deleteAll();
