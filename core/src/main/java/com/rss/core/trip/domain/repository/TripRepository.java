04a929b6cc2be0c9cb364c4e41384952bc04ca88
Hamza Elzarw <hamzaelzarw@gmail.com>
2025-09-15 20:19:54 +0300

    Fix concurrent driver acceptTrip
    Signed-off-by: Hamza Elzarw <hamzaelzarw@gmail.com>

--- core/src/main/java/com/rss/core/trip/domain/repository/TripRepository.java
+++ core/src/main/java/com/rss/core/trip/domain/repository/TripRepository.java
@@ -5,6 +5,9 @@ import com.rss.core.trip.domain.entity.Trip.TripStatus;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -18,4 +21,8 @@ public interface TripRepository extends JpaRepository<Trip, Long> {
     boolean existsByDriverIdAndStatusNotIn(Long driverId, List<TripStatus> tripStatus);
 
     boolean existsByRiderIdAndStatusNotIn(Long riderId, List<TripStatus> tripStatus);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("UPDATE Trip t SET t.driverId = :driverId, t.status = com.rss.core.trip.domain.entity.Trip$TripStatus.PICKING_UP WHERE t.id = :tripId AND t.status = com.rss.core.trip.domain.entity.Trip$TripStatus.MATCHING AND t.driverId IS NULL")
+    int acceptTripIfMatching(@Param("tripId") Long tripId, @Param("driverId") Long driverId);
 }
